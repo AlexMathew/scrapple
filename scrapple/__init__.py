e79bd2f60634860fcc77f6554efbb34dc8b4a683
Alex Mathew <alexmathew003@gmail.com>
2015-01-13 11:34:37 +0530

    Set up scrapple init

---
+++ scrapple/__init__.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+"""
+Scrapple
+========
+
+Scrapple is a framework for building and running web scrapers/crawlers according to 
+key-value pair based configuration files.
+
+:copyright: 2015 Alex Mathew, see AUTHORS for more details
+:license: MIT, see LICENSE for more details
+"""
+
+__author__ = "Alex Mathew, and Harish Balakrishnan"
+__copyright__ = "Copyright 2015, Alex Mathew"
+__credits__ = ["Alex Mathew", "Harish Balakrishnan"]
+__license__ = "MIT"
+__version__ = "0.1"
+__status__ = "Development"
+__maintainer__ = "Alex Mathew"
+__email__ = "alexmathew003@gmail.com"
+
