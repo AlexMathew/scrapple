e62a6f0ef96e08bdbadecbe22fe0be2a599b8353
Alex Mathew <alexmathew003@gmail.com>
2015-01-17 16:06:48 +0530

    Switch CLI to docopt

--- scrapple/cmd.py
+++ scrapple/cmd.py
@@ -1,41 +1,31 @@
+"""
+Usage:
+    scrapple (-h | --help | --version)
+    scrapple genconfig <projectname> <url> [--type=<type>] [--selector=<selector>]
+    scrapple run <projectname> <output_filename> [--output_type=<output_type>]
+    scrapple generate <projectname> <output_filename> [--output_type=<output_type>]
+    scrapple web
+
+Options:
+    -h, --help
+        Show this help message and exit
+    --version
+        Display the version of Scrapple
+    --type=<type>, -t <type>
+        Specifies if the script generated is a page scraper or a crawler [default: scraper]
+    --selector=<selector>, -s <selector>
+        Specifies if XPath expressions or CSS selectors are used [default: xpath]
+    --output_type=<output_type>, -o <output_type>
+        Specifies if the generated output is stored as CSV or JSON [default: json]
+"""
+
 from __future__ import print_function
-import sys
-import argparse
-import textwrap
+from docopt import docopt
+
 
 def run():
-    parser = argparse.ArgumentParser(
-        prog='scrapple',
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        usage='%(prog)s [genconfig | run | generate | web] project_name',
-        description=textwrap.dedent(open('files/description.txt').read()),
-        epilog=textwrap.dedent(open('files/epilog.txt').read()))
-    parser.add_argument('-V', '--version', action='version', version='%(prog)s 0.1')
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument(
-        "-v", 
-        "--verbose", 
-        action="store_true",
-        help="Show detailed output"
-        )
-    group.add_argument(
-        "-q", 
-        "--quiet", 
-        action="store_true",
-        help="Show brief output"
-        )
-    parser.add_argument(
-        "action", 
-        help=open('files/option_help.txt').read(),
-        choices=['genconfig', 'run', 'generate', 'web']
-        )
-    parser.add_argument(
-        "project_name", 
-        help=open('files/projectname_help.txt').read(),
-        nargs='?',
-        default='scrappleproject'
-        )
-    args = parser.parse_args()
+    args = docopt(__doc__, version='0.1')
+
 
 if __name__ == '__main__':
     run()
