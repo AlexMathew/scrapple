585f33d431775c3c79079bb75f2d17c31c51af1b
Alex Mathew <alexmathew003@gmail.com>
2015-01-19 02:48:01 +0530

    Include execution for genconfig

--- scrapple/commands/genconfig.py
+++ scrapple/commands/genconfig.py
@@ -4,6 +4,10 @@ scrapple.commands.genconfig
 
 """
 
+from jinja2 import Template
+import os
+import json
+
 from . import command
 
 class GenconfigCommand(command.Command):
@@ -18,4 +22,16 @@ class GenconfigCommand(command.Command):
         """
         Execution method of :command: genconfig
         """
-        pass
+        directory = os.path.join(os.getcwd(), 'templates', 'configs')
+        with open(os.path.join(directory, self.args['--type'] + '.txt'), 'r') as f:
+            template_content = f.read()
+        template = Template(template_content)
+        settings = {
+            'projectname': self.args['<projectname>'],
+            'selector_type': self.args['--selector'],
+            'url': self.args['<url>']
+        }
+        rendered = template.render(settings=settings)
+        json_content = eval(rendered)
+        with open(self.args['<projectname>'] + '.json', 'w') as f:
+            json.dump(json_content, f)
