90231d060f0c8158dd56079650a3e0c409c86b02
Alex Mathew <alexmathew003@gmail.com>
2015-01-18 23:14:45 +0530

    Add basic execution of web command

--- scrapple/commands/web.py
+++ scrapple/commands/web.py
@@ -4,12 +4,19 @@ scrapple.commands.web
 
 """
 
+from __future__ import print_function
+from flask import Flask, render_template
+import webbrowser
+from multiprocessing import Process
+
 from . import command
 
 class WebCommand(command.Command):
     """
     Defines the execution of :command: web
     """
+
+    app = Flask(__name__)
 
     def __init__(self, args):
         self.args = args
@@ -18,5 +25,12 @@ class WebCommand(command.Command):
         """
         Execution method of :command: web
         """
-        pass
+        p1 = Process(target = lambda : WebCommand.app.run(host="127.0.0.1", port=5000))
+        p2 = Process(target = lambda : webbrowser.open('http://127.0.0.1:5000'))
+        p1.start()
+        p2.start()
         
+        
+    @app.route('/')
+    def home():
+        return render_template('home.html')
