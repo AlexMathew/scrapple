5ffad17a0c9eb56fac04d4fcfc9c41ac26853116
Alex Mathew <alexmathew003@gmail.com>
2015-02-02 22:44:45 +0530

    Set up basic implementation of run command

--- scrapple/commands/run.py
+++ scrapple/commands/run.py
@@ -4,7 +4,11 @@ scrapple.commands.run
 
 """
 
-from . import command
+from __future__ import print_function
+import json
+from colorama import init, Fore, Back
+
+from scrapple.commands import command
 
 class RunCommand(command.Command):
     """
@@ -18,5 +22,14 @@ class RunCommand(command.Command):
         """
         Execution method of :command: run
         """
-        pass
+        print(Back.GREEN + Fore.BLACK + "Scrapple Run")
+        print(Back.RESET + Fore.RESET)
+        try:
+            with open(self.args['<projectname>'] + '.json', 'r') as f:
+                config = json.load(f)
+            
+        except IOError:
+            print(Back.WHITE + Fore.RED + self.args['<projectname>'], ".json does not ", \
+                  "exist. Use ``scrapple genconfig``." + Back.RESET + Fore.RESET, sep="")
+
         
