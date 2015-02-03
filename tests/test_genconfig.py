223a16075889c8d42b9dcf2f0d6a73194f2109b1
Alex Mathew <alexmathew003@gmail.com>
2015-02-03 18:41:12 +0530

    Fix tests

--- tests/test_genconfig.py
+++ tests/test_genconfig.py
@@ -10,27 +10,27 @@ doc = cmd.__doc__
 
 
 def test_if_genconfig_instance_created():
-	args = docopt(doc, "genconfig project1 www.google.com")
+	args = docopt(doc, "genconfig project1 http://www.google.com")
 	gc = genconfig.GenconfigCommand(args)
 	assert_is_instance(gc, genconfig.GenconfigCommand)
 
 
 def test_if_template_filled():
-	args = docopt(doc, "genconfig project1 www.google.com -s css")
+	args = docopt(doc, "genconfig project1 http://www.google.com -s css")
 	gc = genconfig.GenconfigCommand(args)
 	gc.execute_command()
 	with open(os.path.join(os.getcwd(), 'project1.json'), 'r') as f:
 		config = json.load(f)
 	assert_equals(config['project_name'], "project1")
 	assert_equals(config['selector_type'], "css")
-	assert_equals(config['scraping']['url'], "www.google.com")
+	assert_equals(config['scraping']['url'], "http://www.google.com")
 
 
 def test_crawler_generation():
-	args = docopt(doc, "genconfig project1 www.google.com -t crawler")
+	args = docopt(doc, "genconfig project1 http://www.google.com -t crawler")
 	gc = genconfig.GenconfigCommand(args)
 	gc.execute_command()
 	with open(os.path.join(os.getcwd(), 'project1.json'), 'r') as f:
 		config = json.load(f)
-	next = {"follow_link": "", "data": [{"field": "", "selector": ""}]}
+	next = {"follow_link": "", "data": [{"field": "", "selector": "", "attr": ""}]}
 	assert_dict_equal(config['scraping']['next'], next)
