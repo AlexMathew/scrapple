1df7dd264c6a2b21554b3e3b57b9cb3cddfaa4df
Alex Mathew <alexmathew003@gmail.com>
2015-02-03 12:58:50 +0530

    Set up selectorClass option in run command

--- scrapple/commands/run.py
+++ scrapple/commands/run.py
@@ -9,6 +9,7 @@ import json
 from colorama import init, Fore, Back
 
 from scrapple.commands import command
+from scrapple.selectors import xpath, css
 
 class RunCommand(command.Command):
     """
@@ -17,6 +18,7 @@ class RunCommand(command.Command):
 
     def __init__(self, args):
         self.args = args
+        init()
 
     def execute_command(self):
         """
@@ -26,10 +28,27 @@ class RunCommand(command.Command):
         print(Back.RESET + Fore.RESET)
         try:
             with open(self.args['<projectname>'] + '.json', 'r') as f:
-                config = json.load(f)
-            
+                self.config = json.load(f)
+            runner = getattr(self, self.args['--type'])
+            runner()
         except IOError:
             print(Back.WHITE + Fore.RED + self.args['<projectname>'], ".json does not ", \
                   "exist. Use ``scrapple genconfig``." + Back.RESET + Fore.RESET, sep="")
 
-        
+
+    def scraper(self):
+        selectorClass = getattr(
+                eval(self.config['selector_type']), 
+                self.config['selector_type'].title() + 'Selector'
+                )
+        selector = selectorClass(self.config['scraping']['url'])
+        return
+
+
+    def crawler(self):
+        selectorClass = getattr(
+                eval(self.config['selector_type']), 
+                self.config['selector_type'].title() + 'Selector'
+                )
+        selector = selectorClass(self.config['scraping']['url'])
+        return
