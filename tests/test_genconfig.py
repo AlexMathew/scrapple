3ac1f2cd23110809a0a40c7e1f017a011c03de19
Alex Mathew <alexmathew003@gmail.com>
2015-02-12 14:40:42 +0530

    Fix tests

--- tests/test_genconfig.py
+++ tests/test_genconfig.py
@@ -1,4 +1,4 @@
-from nose.tools import assert_equals, assert_dict_equal, assert_is_instance
+from nose.tools import assert_equals, assert_list_equal, assert_is_instance
 from docopt import docopt
 import json
 import os
@@ -10,27 +10,34 @@ doc = cmd.__doc__
 
 
 def test_if_genconfig_instance_created():
-	args = docopt(doc, "genconfig project1 http://www.google.com")
+	args = docopt(doc, "genconfig trial1 http://www.google.com")
 	gc = genconfig.GenconfigCommand(args)
 	assert_is_instance(gc, genconfig.GenconfigCommand)
 
 
 def test_if_template_filled():
-	args = docopt(doc, "genconfig project1 http://www.google.com -s css")
+	args = docopt(doc, "genconfig trial1 http://www.google.com -s css")
 	gc = genconfig.GenconfigCommand(args)
 	gc.execute_command()
-	with open(os.path.join(os.getcwd(), 'project1.json'), 'r') as f:
+	with open(os.path.join(os.getcwd(), 'trial1.json'), 'r') as f:
 		config = json.load(f)
-	assert_equals(config['project_name'], "project1")
+	assert_equals(config['project_name'], "trial1")
 	assert_equals(config['selector_type'], "css")
 	assert_equals(config['scraping']['url'], "http://www.google.com")
 
 
 def test_crawler_generation():
-	args = docopt(doc, "genconfig project1 http://www.google.com -t crawler")
+	args = docopt(doc, "genconfig trial1 http://www.google.com -t crawler")
 	gc = genconfig.GenconfigCommand(args)
 	gc.execute_command()
-	with open(os.path.join(os.getcwd(), 'project1.json'), 'r') as f:
+	with open(os.path.join(os.getcwd(), 'trial1.json'), 'r') as f:
 		config = json.load(f)
-	next = {"follow_link": "", "data": [{"field": "", "selector": "", "attr": ""}]}
-	assert_dict_equal(config['scraping']['next'], next)
+	next = [
+		{
+			"follow_link": "", 
+			"scraping": {
+				"data": [{"field": "", "selector": "", "attr": ""}]
+			}
+		}
+	]
+	assert_list_equal(config['scraping']['next'], next)
