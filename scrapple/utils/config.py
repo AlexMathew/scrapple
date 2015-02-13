23cf747a3ff24f75d3300547f4bfdecf10c4a325
Alex Mathew <alexmathew003@gmail.com>
2015-02-13 19:33:43 +0530

    Add next traversal util function

---
+++ scrapple/utils/config.py
@@ -0,0 +1,24 @@
+"""
+scrapple.utils.config
+~~~~~~~~~~~~~~~~~~~~~
+
+Functions related to traversing the configuration file
+"""
+
+from __future__ import print_function
+
+
+def traverse_next(page, next, results):
+    for link in page.extract_links(next['follow_link']):
+        print("Loading page", link.url)
+        r = results
+        for attribute in next['scraping'].get('data'):
+            if attribute['field'] != "":
+                print("\nExtracting", attribute['field'], "attribute", sep=' ')
+                r[attribute['field']] = link.extract_content(attribute['selector'], attribute['attr'])
+        if not next['scraping'].get('next'):
+            yield r
+        else:
+            for next2 in next['scraping'].get('next'):
+                for result in traverse_next(link, next2, r):
+                    yield result
