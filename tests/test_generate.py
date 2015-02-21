81672389ad5bfedf7f492c9b3c0e4ab35c2870e1
Alex Mathew <alexmathew003@gmail.com>
2015-02-21 18:56:29 +0530

    Add test for error cases

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -8,6 +8,25 @@ from scrapple.commands import generate
 doc = cmd.__doc__
 
 
+import sys
+from contextlib import contextmanager
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+
+@contextmanager
+def output():
+	new_out = StringIO()
+	old_out = sys.stdout
+	try:
+		sys.stdout = new_out
+		yield sys.stdout
+	finally:
+		sys.stdout = old_out
+
+
 def test_if_generate_instance_created():
 	args = docopt(doc, "generate project1 test_project")
 	gc = generate.GenerateCommand(args)
@@ -32,3 +51,13 @@ def test_css_scraper_generate():
 		program = f.read()
 	assert_in("from scrapple.selectors.css import CssSelector", program)
 	assert_in('page0 = CssSelector("http://www.google.com")', program)
+
+
+def test_nonexistent_project():
+	args = docopt(doc, "generate project_unknown project2")
+	gc = generate.GenerateCommand(args)
+	with output() as out:
+		gc.execute_command()
+	alert = out.getvalue().strip()
+	expected_alert = "project_unknown.json does not exist. Use ``scrapple genconfig``."
+	assert_in(expected_alert, alert)
