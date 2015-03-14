f205e5799a4f3e4126c01bc3badbc632cefcaf0d
Alex Mathew <alexmathew003@gmail.com>
2015-03-14 18:47:38 +0530

    Add docstring for get_command_class()

--- scrapple/utils/dynamicdispatch.py
+++ scrapple/utils/dynamicdispatch.py
@@ -6,6 +6,12 @@ Functions related to dynamic dispatch of objects
 """
 
 def get_command_class(command):
+	"""
+	get_command_class
+	-----------------
+
+	Called from runCLI() to select the command class for the selected command.
+	"""
     from scrapple.commands import genconfig, generate, run, web
     cmdClass = getattr(eval(command), command.title() + 'Command')
     return cmdClass
