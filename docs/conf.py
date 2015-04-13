0f6daa85833df6aa47ce481b52d571d8e40c74f0
Alex Mathew <alexmathew003@gmail.com>
2015-04-13 22:01:27 +0530

    Set up 0.2.4

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ copyright = u'2014, Alex Mathew, Harish Balakrishnan'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.3'
+version = '0.2.4'
 # The full version, including alpha/beta/rc tags.
 release = ''
 
